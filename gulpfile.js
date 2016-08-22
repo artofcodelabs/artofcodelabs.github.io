c66f84015435354f1e845f504b284cc947f9e550
himn1 <zbigniew.humeniuk@gmail.com>
2016-08-22 12:09:42 +0200

    Gulpfile changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,48 @@
-/*!
- * gulp
- * $ npm install gulp-coffee gulp-concat gulp-notify del gulp-jasmine-browser gulp-watch --save-dev
- */
-
-/*
-  $ gulp         - start build & watch tasks
-  $ gulp watch   - watch .coffee files for changes
-  $ gulp build   - build
-*/
+console.log('Node version: ' + process.version);
 
 // Load plugins
 var gulp = require('gulp'),
+    del = require('del'),
+    pump = require('pump'),
     gutil = require('gulp-util'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
-    del = require('del'),
     coffee = require('gulp-coffee'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    sass = require('gulp-sass'),
+    size = require('gulp-size'),
+    prefix = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    imagemin = require('gulp-imagemin'),
+    csslint = require('gulp-csslint'),
+    shell = require('gulp-shell'),
+    uglify = require('gulp-uglify');
+
+// BEGIN Main tasks
 
 // Default task
 gulp.task('default', ['build'], function() {
   gulp.start('watch');
 });
 
+// Production task
+gulp.task('production', function(){
+  gulp.run('minify-images');
+});
+
+// END Main tasks
+
 // Build task
 gulp.task('build', function() {
-  gulp.start('scripts');
+  gulp.start('minify-js');
+  gulp.start('minify-css');
 });
 
 // Watch
 gulp.task('watch', function() {
-  gulp.watch(['_coffeescripts/**/*.coffee'], {cwd: '.'}, ['scripts']);
+  gulp.watch(['_coffeescripts/**/*.coffee'], {cwd: '.'}, ['minify-js']);
+  gulp.watch(['_sass/**/*.scss'], {cwd: '.'}, ['minify-css']);
 });
 
 // Clean
@@ -62,6 +74,57 @@ gulp.task('scripts', ['coffee'], function() {
   ];
   return gulp.src(manifest)
     .pipe(concat('application.js'))
-    .pipe(gulp.dest('./javascripts/'))
-    .pipe(notify({ message: 'Scripts task complete' }));
+    .pipe(gulp.dest('./javascripts/'));
 });
+
+gulp.task('minify-js', ['scripts'], function (cb) {
+  pump([
+    gulp.src('./javascripts/application.js'),
+    uglify(),
+    rename('application.min.js'),
+    gulp.dest('./javascripts/'),
+    notify({message: 'JavaScript created'})
+  ], cb);
+});
+
+// Task that compiles scss files down to good old css
+gulp.task('pre-process', function(){
+  return gulp.src('./_sass/application.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(size({gzip: false, showFiles: true, title:'without vendor prefixes'}))
+    .pipe(size({gzip: true, showFiles: true, title:'without vendor prefixes'}))
+    .pipe(prefix())
+    .pipe(size({gzip: false, showFiles: true, title:'after vendor prefixes'}))
+    .pipe(size({gzip: true, showFiles: true, title:'after vendor prefixes'}))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('minify-css', ['pre-process'], function(){
+  return gulp.src('./css/application.css')
+    .pipe(minifyCSS())
+    .pipe(size({gzip: false, showFiles: true, title:'minified css'}))
+    .pipe(size({gzip: true, showFiles: true, title:'minified css'}))
+    .pipe(rename('application.min.css'))
+    .pipe(gulp.dest('./css/'))
+    .pipe(notify({message: 'CSS created'}));
+});
+
+gulp.task('minify-images', function(){
+  return gulp.src('./images/*')
+    .pipe(size({gzip: false, showFiles: true, title:'original image size'}))
+    .pipe(size({gzip: true, showFiles: true, title:'original image size'}))
+    .pipe(imagemin())
+    .pipe(size({gzip: false, showFiles: true, title:'minified images'}))
+    .pipe(size({gzip: true, showFiles: true, title:'minified images'}))
+    .pipe(gulp.dest('./images'));
+});
+
+gulp.task('csslint', function(){
+  return gulp.src('./css/application.css')
+    .pipe(csslint())
+    .pipe(csslint.formatter());
+});
+
+gulp.task('jekyll', shell.task([
+  'jekyll s'
+]));
