cfda464bd4ef8d5ab1529dc4e4a4a218d36a0eee
himn1 <zbigniew.humeniuk@gmail.com>
2017-11-16 12:15:05 +0100

    In progress

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './_src/index.js',
+  entry: {
+    application: './_src/app.js'
+  },
   module: {
     rules: [
 
     ]
   },
+  plugins: [
+    new CleanWebpackPlugin(['assets'], {
+      exclude: [],
+      verbose: true,
+      dry: false
+    })
+  ],
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'assets')
