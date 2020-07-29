38e9d5fa3583bd1f5006deccd60daa8c87790b8c
Zbigniew Humeniuk <hello@artofcode.co>
2020-07-29 07:48:21 +0200

    [internal] refactoring

--- _src/app.js
+++ _src/app.js
@@ -4,17 +4,13 @@ import 'script-loader!jquery';
 import 'script-loader!foundation-sites/dist/plugins/foundation.core.js';
 import 'script-loader!foundation-sites/dist/plugins/foundation.util.mediaQuery.js';
 
-import { init, Controllers } from 'loco-js-core';
+import { init } from 'loco-js-core';
 import Main from './js/controllers/main';
 
 import './css/application.scss';
 
-Object.assign(Controllers, {
-  Main
-});
-
 $(document).foundation();
 
 document.addEventListener("DOMContentLoaded", function() {
-  init(Controllers);
+  init({ Main });
 });
