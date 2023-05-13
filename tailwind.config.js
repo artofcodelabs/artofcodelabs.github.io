647b5da2708da57a7430b570feb9d294a2f4653b
Zbig <hello@artofcode.co>
2023-05-14 00:51:11 +0200

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./*.html", "./_includes/**/*.html", "./_src/**/*.js"],
+  content: [
+    "./*.html",
+    "./_layouts/**/*.html",
+    "./_includes/**/*.html",
+    "./_src/**/*.js",
+  ],
   theme: {
     extend: {},
   },
